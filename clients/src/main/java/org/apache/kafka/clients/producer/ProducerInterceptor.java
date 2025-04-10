406a45cac3a2ef661def9d6135c9e25bef90da87
wangxinglei <18868823172@163.com>
2025-04-10 20:00:18 +0800

    ProducerInterceptor

--- clients/src/main/java/org/apache/kafka/clients/producer/ProducerInterceptor.java
+++ clients/src/main/java/org/apache/kafka/clients/producer/ProducerInterceptor.java
@@ -19,79 +19,72 @@ package org.apache.kafka.clients.producer;
 import org.apache.kafka.common.Configurable;
 
 /**
- * A plugin interface that allows you to intercept (and possibly mutate) the records received by the producer before
- * they are published to the Kafka cluster.
+ * 生产者拦截器接口，允许在消息发送到Kafka集群之前拦截(并可能修改)生产者收到的记录。
+ * 这个接口主要用于第三方组件对生产者应用进行自定义监控、日志记录等操作。
  * <p>
- * This class will get producer config properties via <code>configure()</code> method, including clientId assigned
- * by KafkaProducer if not specified in the producer config. The interceptor implementation needs to be aware that it will be
- * sharing producer config namespace with other interceptors and serializers, and ensure that there are no conflicts.
+ * 该类通过configure()方法获取生产者配置属性，包括KafkaProducer分配的clientId(如果生产者配置中未指定)。
+ * 拦截器实现需要注意它将与其他拦截器和序列化器共享生产者配置命名空间，并确保不存在冲突。
  * <p>
- * Exceptions thrown by ProducerInterceptor methods will be caught, logged, but not propagated further. As a result, if
- * the user configures the interceptor with the wrong key and value type parameters, the producer will not throw an exception,
- * just log the errors.
+ * ProducerInterceptor方法抛出的异常将被捕获并记录日志，但不会进一步传播。因此，如果用户配置了错误的key和value类型参数，
+ * 生产者不会抛出异常，只会记录错误。
  * <p>
- * ProducerInterceptor callbacks may be called from multiple threads. Interceptor implementation must ensure thread-safety, if needed.
+ * ProducerInterceptor的回调可能会从多个线程调用。如果需要，拦截器实现必须确保线程安全。
  * <p>
- * Implement {@link org.apache.kafka.common.ClusterResourceListener} to receive cluster metadata once it's available. Please see the class documentation for ClusterResourceListener for more information.
- * Implement {@link org.apache.kafka.common.metrics.Monitorable} to enable the interceptor to register metrics. The following tags are automatically added to
- * all metrics registered: <code>config</code> set to <code>interceptor.classes</code>, and <code>class</code> set to the ProducerInterceptor class name.
+ * 实现{@link org.apache.kafka.common.ClusterResourceListener}接口可以在集群元数据可用时接收通知。
+ * 更多信息请参见ClusterResourceListener的类文档。
+ * 实现{@link org.apache.kafka.common.metrics.Monitorable}接口可以使拦截器注册度量指标。
+ * 所有注册的度量指标会自动添加以下标签：config设置为interceptor.classes，class设置为ProducerInterceptor类名。
  */
 public interface ProducerInterceptor<K, V> extends Configurable, AutoCloseable {
     /**
-     * This is called from {@link org.apache.kafka.clients.producer.KafkaProducer#send(ProducerRecord)} and
-     * {@link org.apache.kafka.clients.producer.KafkaProducer#send(ProducerRecord, Callback)} methods, before key and value
-     * get serialized and partition is assigned (if partition is not specified in ProducerRecord).
+     * 该方法在{@link org.apache.kafka.clients.producer.KafkaProducer#send(ProducerRecord)}和
+     * {@link org.apache.kafka.clients.producer.KafkaProducer#send(ProducerRecord, Callback)}方法中调用，
+     * 在key和value被序列化以及分区被分配之前(如果ProducerRecord中未指定分区)。
      * <p>
-     * This method is allowed to modify the record, in which case, the new record will be returned. The implication of modifying
-     * key/value is that partition assignment (if not specified in ProducerRecord) will be done based on modified key/value,
-     * not key/value from the client. Consequently, key and value transformation done in onSend() needs to be consistent:
-     * same key and value should mutate to the same (modified) key and value. Otherwise, log compaction would not work
-     * as expected.
+     * 该方法允许修改记录，这种情况下，将返回新的记录。修改key/value的含义是分区分配(如果ProducerRecord中未指定)
+     * 将基于修改后的key/value进行，而不是客户端的原始key/value。因此，onSend()中进行的key和value转换需要保持一致：
+     * 相同的key和value应该转换为相同的(修改后的)key和value。否则，日志压缩可能无法按预期工作。
      * <p>
-     * Similarly, it is up to interceptor implementation to ensure that correct topic/partition is returned in ProducerRecord.
-     * Most often, it should be the same topic/partition from 'record'.
+     * 同样，拦截器实现需要确保在ProducerRecord中返回正确的主题/分区。
+     * 通常情况下，应该与'record'中的主题/分区保持一致。
      * <p>
-     * Any exception thrown by this method will be caught by the caller and logged, but not propagated further.
+     * 该方法抛出的任何异常都将被调用者捕获并记录日志，但不会进一步传播。
      * <p>
-     * Since the producer may run multiple interceptors, a particular interceptor's onSend() callback will be called in the order
-     * specified by {@link org.apache.kafka.clients.producer.ProducerConfig#INTERCEPTOR_CLASSES_CONFIG}. The first interceptor
-     * in the list gets the record passed from the client, the following interceptor will be passed the record returned by the
-     * previous interceptor, and so on. Since interceptors are allowed to modify records, interceptors may potentially get
-     * the record already modified by other interceptors. However, building a pipeline of mutable interceptors that depend on the output
-     * of the previous interceptor is discouraged, because of potential side-effects caused by interceptors potentially failing to
-     * modify the record and throwing an exception. If one of the interceptors in the list throws an exception from onSend(), the exception
-     * is caught, logged, and the next interceptor is called with the record returned by the last successful interceptor in the list,
-     * or otherwise the client.
+     * 由于生产者可能运行多个拦截器，特定拦截器的onSend()回调将按照
+     * {@link org.apache.kafka.clients.producer.ProducerConfig#INTERCEPTOR_CLASSES_CONFIG}中指定的顺序调用。
+     * 列表中的第一个拦截器获取来自客户端的记录，后续拦截器将获取前一个拦截器返回的记录，依此类推。
+     * 由于拦截器可以修改记录，因此拦截器可能会获取到已被其他拦截器修改的记录。
+     * 但是不建议构建依赖于前一个拦截器输出的可变拦截器管道，因为拦截器可能无法修改记录并抛出异常，这可能会产生副作用。
+     * 如果列表中的某个拦截器从onSend()抛出异常，异常将被捕获并记录日志，下一个拦截器将使用列表中最后一个成功的拦截器返回的记录，
+     * 或者使用客户端的原始记录。
      *
-     * @param record the record from client or the record returned by the previous interceptor in the chain of interceptors.
-     * @return producer record to send to topic/partition
+     * @param record 来自客户端的记录或拦截器链中前一个拦截器返回的记录
+     * @return 要发送到主题/分区的生产者记录
      */
     ProducerRecord<K, V> onSend(ProducerRecord<K, V> record);
 
     /**
-     * This method is called when the record sent to the server has been acknowledged, or when sending the record fails before
-     * it gets sent to the server.
+     * 当记录被服务器确认，或在记录发送到服务器之前发送失败时调用此方法。
      * <p>
-     * This method is generally called just before the user callback is called, and in additional cases when <code>KafkaProducer.send()</code>
-     * throws an exception.
+     * 此方法通常在用户回调之前调用，以及在<code>KafkaProducer.send()</code>抛出异常的其他情况下调用。
      * <p>
-     * Any exception thrown by this method will be ignored by the caller.
+     * 此方法抛出的任何异常都将被调用者忽略。
      * <p>
-     * This method will generally execute in the background I/O thread, so the implementation should be reasonably fast.
-     * Otherwise, sending of messages from other threads could be delayed.
+     * 此方法通常在后台I/O线程中执行，因此实现应该合理快速。
+     * 否则，可能会延迟其他线程的消息发送。
      *
-     * @param metadata The metadata for the record that was sent (i.e. the partition and offset).
-     *                 If an error occurred, metadata will contain only valid topic and maybe
-     *                 partition. If partition is not given in ProducerRecord and an error occurs
-     *                 before partition gets assigned, then partition will be set to RecordMetadata.NO_PARTITION.
-     *                 The metadata may be null if the client passed null record to
-     *                 {@link org.apache.kafka.clients.producer.KafkaProducer#send(ProducerRecord)}.
-     * @param exception The exception thrown during processing of this record. Null if no error occurred.
+     * @param metadata 已发送记录的元数据(即分区和偏移量)。
+     *                 如果发生错误，元数据将只包含有效的主题，可能还包含分区。
+     *                 如果ProducerRecord中未给出分区，并且在分配分区之前发生错误，
+     *                 则分区将设置为RecordMetadata.NO_PARTITION。
+     *                 如果客户端向{@link org.apache.kafka.clients.producer.KafkaProducer#send(ProducerRecord)}传递了null记录，
+     *                 则元数据可能为null。
+     * @param exception 处理此记录期间抛出的异常。如果没有发生错误则为null。
      */
     void onAcknowledgement(RecordMetadata metadata, Exception exception);
 
     /**
-     * This is called when interceptor is closed
+     * 当拦截器关闭时调用此方法
      */
     void close();
 }
