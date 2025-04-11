b7628c9b22f87e058d37d8d1163aa0788b2e458d
王星磊 <wangxinglei@yonyou.com>
2025-04-11 09:24:19 +0800

    BufferExhaustedException

--- clients/src/main/java/org/apache/kafka/clients/producer/BufferExhaustedException.java
+++ clients/src/main/java/org/apache/kafka/clients/producer/BufferExhaustedException.java
@@ -19,12 +19,21 @@ package org.apache.kafka.clients.producer;
 import org.apache.kafka.common.errors.TimeoutException;
 
 /**
- * This exception is thrown if the producer cannot allocate memory for a record within max.block.ms due to the buffer
- * being too full.
+ * 当Kafka生产者无法在max.block.ms配置的时间内为新记录分配内存时，会抛出此异常。
+ * 这种情况通常发生在生产者的缓冲区已满的时候。
  *
- * In earlier versions a TimeoutException was thrown instead of this. To keep existing catch-clauses working
- * this class extends TimeoutException.
+ * 生产者的缓冲区大小由buffer.memory参数控制，用于存储尚未发送到服务器的记录。
+ * 当生产者的发送速度超过了向Kafka服务器发送数据的速度时，记录会积累在缓冲区中。
+ * 如果缓冲区被填满，新的send()调用将被阻塞max.block.ms毫秒。
+ * 如果在此时间内无法获得足够的缓冲区空间，则会抛出此异常。
  *
+ * 在早期版本中，这种情况下会抛出TimeoutException。
+ * 为了保持向后兼容性并确保现有的异常处理代码能继续工作，
+ * 此类继承自TimeoutException。
+ *
+ * 相关配置参数：
+ * - buffer.memory：设置生产者可用于缓冲等待发送到服务器的记录的内存大小
+ * - max.block.ms：send()方法和partitionsFor()方法阻塞的最大时间
  */
 public class BufferExhaustedException extends TimeoutException {
 
